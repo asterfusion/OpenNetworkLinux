041ab2636215a96acea818be841d05fde2d4fe8a
tsihang <tsihang@asterfusion.com>
2023-09-07 16:50:17 +0800

    New platform

---
+++ packages/platforms/asterfusion/x86-64/x732q_t/onlp/builds/x86_64_asterfusion_x732q_t/module/auto/make.mk
@@ -0,0 +1,7 @@
+#
+# x86_64_asterfusion_x732q_t Autogeneration
+#
+###############################################################################
+x86_64_asterfusion_x732q_t_DEFS := module/auto/x86_64_asterfusion_x732q_t.yml
+x86_64_asterfusion_x732q_t_DIRS := module/inc/x86_64_asterfusion_x732q_t module/src
+include $(BUILDER)/auto.mk
---
+++ packages/platforms/asterfusion/x86-64/x732q_t/onlp/builds/x86_64_asterfusion_x732q_t/module/inc/x86_64_asterfusion_x732q_t/x86_64_asterfusion_x732q_t_dox.h
@@ -0,0 +1,26 @@
+/**************************************************************************//**
+ *
+ * x86_64_asterfusion_x732q_t Doxygen Header
+ *
+ *****************************************************************************/
+#ifndef __x86_64_asterfusion_x732q_t_DOX_H__
+#define __x86_64_asterfusion_x732q_t_DOX_H__
+
+/**
+ * @defgroup x86_64_asterfusion_x732q_t x86_64_asterfusion_x732q_t - x86_64_asterfusion_x732q_t Description
+ *
+
+The documentation overview for this module should go here.
+
+ *
+ * @{
+ *
+ * @defgroup x86_64_asterfusion_x732q_t-x86_64_asterfusion_x732q_t Public Interface
+ * @defgroup x86_64_asterfusion_x732q_t-config Compile Time Configuration
+ * @defgroup x86_64_asterfusion_x732q_t-porting Porting Macros
+ *
+ * @}
+ *
+ */
+
+#endif /* __x86_64_asterfusion_x732q_t_DOX_H__ */
